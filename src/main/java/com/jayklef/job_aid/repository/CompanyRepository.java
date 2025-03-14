0e4d5f80a71a718c8901dc59ca249467d0573d10
Jayklef <arhawhojerry@gmail.com>
2025-03-14 14:07:27 +0000

    repository annot

--- src/main/java/com/jayklef/job_aid/repository/CompanyRepository.java
+++ src/main/java/com/jayklef/job_aid/repository/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.jayklef.job_aid.repository;
 
 import com.jayklef.job_aid.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
