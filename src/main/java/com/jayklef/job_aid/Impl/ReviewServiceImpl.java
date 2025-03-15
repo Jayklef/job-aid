efa7ef3b9aa0471dc86be3ec5674b941760459ef
Jayklef <arhawhojerry@gmail.com>
2025-03-15 00:25:34 +0000

    add review

--- src/main/java/com/jayklef/job_aid/Impl/ReviewServiceImpl.java
+++ src/main/java/com/jayklef/job_aid/Impl/ReviewServiceImpl.java
@@ -1,7 +1,9 @@
 package com.jayklef.job_aid.Impl;
 
+import com.jayklef.job_aid.model.Company;
 import com.jayklef.job_aid.model.Review;
 import com.jayklef.job_aid.repository.ReviewRepository;
+import com.jayklef.job_aid.service.CompanyService;
 import com.jayklef.job_aid.service.ReviewService;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +12,29 @@ import java.util.List;
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
+    private final CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
-
         return reviews;
+    }
+
+    @Override
+    public boolean saveReview(Long companyId, Review review) {
+        Company company = companyService.getCompany(companyId);
+        if (company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/com/jayklef/job_aid/controller/ReviewController.java
+++ src/main/java/com/jayklef/job_aid/controller/ReviewController.java
@@ -4,10 +4,7 @@ import com.jayklef.job_aid.model.Review;
 import com.jayklef.job_aid.service.ReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,14 @@ public class ReviewController {
     @GetMapping("/reviews")
     public ResponseEntity<List<Review>> getAllReviews(@PathVariable("companyId") Long companyId){
         return new ResponseEntity<>(reviewService.getAllReviews(companyId), HttpStatus.OK);
+    }
+    @PostMapping("/reviews/save")
+    public ResponseEntity<String> saveReview(@PathVariable("companyId") Long companyId,
+                                             @RequestBody Review review){
+        boolean isReviewSaved = reviewService.saveReview(companyId, review);
+        if (isReviewSaved)
+        return new ResponseEntity<>("review added successfully", HttpStatus.CREATED);
+        else
+            return new ResponseEntity<>("review not saved", HttpStatus.NOT_FOUND);
     }
 }
